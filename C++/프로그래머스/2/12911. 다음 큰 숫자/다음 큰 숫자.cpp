33e86289b3bc345f5375fdfc57e36225ca7476fe
choidahey <81176789+choidahey@users.noreply.github.com>
2025-10-23 01:00:50 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 3.85 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string binary_convert(int n)
+{
+    string binary = "";
+    
+    while (n / 2 != 0)
+    {
+        binary += to_string(n % 2);
+        n /= 2;
+    }
+    
+    reverse(binary.begin(), binary.end());
+    
+    return binary;
+}
+
+int char_counter(string s, char c)
+{
+    int count = 0;
+    
+    for (char a : s)
+    {
+        if (a == c) count++;
+    }
+    
+    return count;
+}
+
+int solution(int n) {
+    int answer = 0;
+    int char_count = char_counter(binary_convert(n), '1');
+    
+    while (n < 1000000)
+    {
+        n++;
+        int temp_count = char_counter(binary_convert(n), '1');
+        if (char_count == temp_count)
+        {
+            answer = n;
+            break;
+        }
+    }
+    
+    return answer;
+}
